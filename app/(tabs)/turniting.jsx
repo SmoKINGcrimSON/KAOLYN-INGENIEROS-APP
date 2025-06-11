7aa2b84ca250519fa2fb12f65b22d127693704bc
smoking <ferchowarrior169@gmail.com>
2025-06-11 10:53:41 -0500

    app completed

--- app/(tabs)/turniting.jsx
+++ app/(tabs)/turniting.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 
@@ -6,18 +6,100 @@ const Turniting = () => {
   const insets = useSafeAreaInsets()
 
   return (
-    <View style = {{
-          paddingTop: insets.top, 
-          paddingBottom: insets.bottom,
-          paddingLeft: insets.left,
-          paddingRight: insets.right
+    <View style={{
+      flex: 1,
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+      backgroundColor: '#F4F8F1',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <StatusBar style='dark' />
+
+      {/* Document Thumbnail */}
+      <View style={{
+        borderWidth: 4,
+        borderColor: '#0C3C0C',
+        borderRadius: 12,
+        padding: 6,
+        backgroundColor: '#ffffff',
+        marginBottom: 20
+      }}>
+        <Image
+          source={require("../../assets/photos/tesis.jpg")}
+          style={{ width: 160, height: 230, resizeMode: 'contain' }}
+        />
+      </View>
+
+      {/* Plagiarism Percentage */}
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10
+      }}>
+        <Text style={{
+          backgroundColor: '#D9E4C7',
+          paddingVertical: 10,
+          paddingHorizontal: 20,
+          borderTopLeftRadius: 10,
+          borderBottomLeftRadius: 10,
+          fontSize: 14,
+          fontWeight: 'bold'
         }}>
-          <StatusBar style='black'/>
-          <Text>Turniting</Text>
+          Porcentaje de plagio
+        </Text>
+        <View style={{
+          backgroundColor: '#D1D7BD',
+          paddingVertical: 10,
+          paddingHorizontal: 20,
+          borderTopRightRadius: 10,
+          borderBottomRightRadius: 10
+        }}>
+          <Text style={{
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: '#0C3C0C'
+          }}>
+            100%
+          </Text>
+        </View>
+      </View>
+
+      {/* Action Buttons */}
+      <TouchableOpacity style={{
+        backgroundColor: '#C8DDB3',
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 10,
+        marginBottom: 10
+      }}>
+        <Text style={{
+          fontWeight: '600',
+          color: '#000',
+          fontSize: 14
+        }}>
+          Descargar informes
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{
+        backgroundColor: '#C8DDB3',
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 10
+      }}>
+        <Text style={{
+          fontWeight: '600',
+          color: '#000',
+          fontSize: 14
+        }}>
+          Subir nuevo documento
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Turniting
-
-const styles = StyleSheet.create({})
